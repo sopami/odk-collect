45692ce53f09d58a31ce1b8896809673c32c9b47
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-08-31 14:48:40 +0200

    Fix images disappearing from the choices list

--- collect_app/src/main/java/org/odk/collect/android/formentry/questions/AudioVideoImageTextLabel.java
+++ collect_app/src/main/java/org/odk/collect/android/formentry/questions/AudioVideoImageTextLabel.java
@@ -129,6 +129,7 @@ public class AudioVideoImageTextLabel extends RelativeLayout implements View.OnC
     public void setImage(@NonNull File imageFile, ImageLoader imageLoader) {
         if (imageFile.exists()) {
             binding.imageView.layout(0, 0, 0, 0);
+            binding.imageView.requestLayout();
 
             imageLoader.loadImage(binding.imageView, imageFile, ImageView.ScaleType.CENTER_INSIDE, null);
             binding.imageView.setVisibility(VISIBLE);
